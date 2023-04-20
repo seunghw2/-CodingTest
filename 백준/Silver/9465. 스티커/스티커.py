fae7e0ed6d137283e7ab44d7344a80a5a76b5fa1
seunghw2 <100360525+seunghw2@users.noreply.github.com>
2023-04-20 12:12:13 +0900

    [Silver I] Title: 스티커, Time: 308 ms, Memory: 171196 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -5,30 +5,18 @@ T = int(input())
 
 for _ in range(T):
     n = int(input())
-    arr = []
-    arr.append(list(map(int, input().split())))
-    arr.append((list(map(int, input().split()))))
+    dp = []
+    dp.append(list(map(int, input().split())))
+    dp.append((list(map(int, input().split()))))
 
     if n == 1:
-        print(max(arr[0][0], arr[1][0]))
-        continue
+        print(max(dp[0][0], dp[1][0]))
+    else:
+        dp[0][1] += dp[1][0]
+        dp[1][1] += dp[0][0]
 
-    dp = [[-1 for _ in range(n)] for _ in range(2)]
+        for x in range(2, n):
+            dp[0][x] += max(dp[1][x-2], dp[1][x-1])
+            dp[1][x] += max(dp[0][x-2], dp[0][x-1])
 
-    dp[0][0] = arr[0][0]
-    dp[1][0] = arr[1][0]
-    dp[0][1] = arr[0][1] + arr[1][0]
-    dp[1][1] = arr[0][0] + arr[1][1]
-
-    ans = max(dp[0][0], dp[0][1], dp[1][0], dp[1][1])
-
-    for x in range(2, n):
-        for y in range(2):
-            if y == 0:
-                dp[y][x] = max(dp[0][x-2], dp[1][x-2], dp[1][x-1]) + arr[y][x]
-            else:
-                dp[y][x] = max(dp[0][x-2], dp[0][x-1], dp[1][x-2]) + arr[y][x]
-            if dp[y][x] > ans:
-                ans = dp[y][x]
-
-    print(ans)
+        print(max(dp[0][-1], dp[1][-1]))
