35cce9bfba125546d7b9fca81cbd46d5aae3ec01
seunghw2 <100360525+seunghw2@users.noreply.github.com>
2023-05-02 18:35:05 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 688 ms, Memory: 128856 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -3,41 +3,54 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static Queue<Integer> queue = new LinkedList<>();
-    static List<List<Integer>> adjList = new ArrayList<>();
-    static int[] order;
-    static int cnt;
-
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int a;
         int b;
-        order = new int[N + 1];
-        Arrays.fill(order, 1);
 
-        for(int i = 0; i <= N; i++){
+        List<List<Integer>> adjList = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int cnt[] = new int[N + 1];
+        int order[] = new int[N + 1];
+        int item;
+
+        Arrays.fill(cnt, 0);
+        Arrays.fill(order, 0);
+        for (int i = 0; i <= N; i++) {
             adjList.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < M; i++){
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
             adjList.get(a).add(b);
+            cnt[b] += 1;
         }
 
-        for(int i = 1; i <= N; i++){
-            for(int item: adjList.get(i)) {
-                order[item] = Math.max(order[item], order[i] + 1);
+        for (int i = 0; i <= N; i++) {
+            if (cnt[i] == 0) {
+                order[i] = 1;
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            item = queue.poll();
+            for (int next : adjList.get(item)) {
+                cnt[next] -= 1;
+                if (cnt[next] == 0) {
+                    order[next] = order[item] + 1;
+                    queue.add(next);
+                }
             }
         }
 
