3321ded250eafa1f724abc4b037debea15141884
seunghw2 <100360525+seunghw2@users.noreply.github.com>
2023-05-19 20:32:08 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 3908 ms, Memory: 213580 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,29 @@
+N = int(input())
+k = 0
+for i in range(8):
+    if pow(3, i) == N:
+        k = i
+        break
+
+arr = [[] for _ in range(k+1)]
+star = [[1 for _ in range(N)] for _ in range(N)]
+
+for i in range(1, k+1):
+    for j in range(pow(3, i-1), 2*pow(3, i-1)):
+        arr[i].append(j)
+
+for y in range(N):
+    for x in range(N):
+        for i in range(1, k+1):
+            if x % pow(3, i) in arr[i] and y % pow(3, i) in arr[i]:
+                star[y][x] = 0
+                break
+
+for line in star:
+    for item in line:
+        if item == 1:
+            print("*", end='')
+        else:
+            print(" ", end='')
+    print()
+
