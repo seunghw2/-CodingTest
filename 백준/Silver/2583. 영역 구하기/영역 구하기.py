ba64ebd358c126c1191c11718e9a55473a08960e
seunghw2 <100360525+seunghw2@users.noreply.github.com>
2023-05-20 18:37:26 +0900

    [Silver I] Title: 영역 구하기, Time: 60 ms, Memory: 32740 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,43 +1,32 @@
 import sys
 sys.setrecursionlimit(10**6)
-
-global M, N, cnt
 M, N, K = map(int, input().split())
-box = []
-visited = set()
-xxyy = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-ans = []
+paper = [[0 for _ in range(N)] for _ in range(M)]
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            paper[i][j] = 1
 
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 def dfs(x, y):
     global cnt
-    if not (x, y) in visited:
-        visited.add((x, y))
-        cnt += 1
-        for xx, yy in xxyy:
-            movx = x + xx
-            movy = y + yy
-            if 0 <= movx < N and 0 <= movy < M:
-                if not (movx, movy) in visited:
-                    dfs(movx, movy)
+    paper[x][y] = 1
+    cnt += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<M and 0<=ny<N and paper[nx][ny] == 0:
+            dfs(nx, ny)
 
-
-for _ in range(K):
-    box.append(list(map(int, input().split())))
-
-for x1, y1, x2, y2 in box:
-    for x in range(x1, x2):
-        for y in range(y1, y2):
-            visited.add((x + 0.5, y + 0.5))
-
-for y in range(M):
-    for x in range(N):
-        cnt = 0
-        dfs(x+0.5, y+0.5)
-        if cnt != 0:
-            ans.append(cnt)
+result = []
+for i in range(M):
+    for j in range(N):
+        if paper[i][j] == 0:
             cnt = 0
+            dfs(i, j)
+            result.append(cnt)
 
-print(len(ans))
-ans.sort()
-for item in ans:
-    print(item, end=' ')
+print(len(result))
+print(*sorted(result))
