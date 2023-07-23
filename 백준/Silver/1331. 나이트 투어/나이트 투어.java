c31eeede2a6d6c996d006e55056c55fd4a8bbd94
seunghw2 <100360525+seunghw2@users.noreply.github.com>
2023-07-23 21:24:53 +0900

    [Silver IV] Title: 나이트 투어, Time: 172 ms, Memory: 16216 KB -BaekjoonHub

---
+++ 백준/Silver/1331. 나이트 투어/나이트 투어.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.System.exit;
+
+class Main {
+    public static boolean isPossible(int[][] arr, int prevRow, int prevCol, int row, int col){
+//        System.out.println(Math.abs(col - prevCol) + " " + Math.abs(row - prevRow));
+        if(Math.abs(col - prevCol) == 2 && Math.abs(row - prevRow) == 1)
+            return true;
+        else if(Math.abs(col - prevCol) == 1 && Math.abs(row - prevRow) == 2)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[6][6];
+        Set<Integer> set = new HashSet<Integer>();
+        String input;
+        int row, col;
+        int firstRow = -1, firstCol = -1;
+        int prevRow = -1, prevCol = -1;
+
+        for(int i = 0; i < 36; i++){
+            input = br.readLine();
+            col = input.charAt(0) - 65;
+            row = 6 - input.charAt(1) + '0';
+
+            if(i == 0){
+                firstRow = row;
+                firstCol = col;
+                prevRow = row;
+                prevCol = col;
+                set.add(row * 6 + col);
+            }
+            else {
+                if (isPossible(arr, prevRow, prevCol, row, col)) {
+                    if (!set.contains(row * 6 + col)) {
+                        prevRow = row;
+                        prevCol = col;
+                        set.add(row * 6 + col);
+                        if (i == 35) {
+                            if (isPossible(arr, firstRow, firstCol, row, col)) {
+                                System.out.println("Valid");
+                                exit(0);
+                            } else
+                                break;
+                        }
+                    } else
+                        break;
+                } else
+                    break;
+            }
+//            System.out.println("[" + prevRow + " , " + prevCol + "] -> " + "[" + row + ", " + col + "]");
+        }
+        System.out.println("Invalid");
+    }
+}
