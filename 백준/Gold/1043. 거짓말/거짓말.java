673e584aa4ed5516f8a9bdee4073001e8b78f447
seunghw2 <100360525+seunghw2@users.noreply.github.com>
2023-08-07 14:28:30 +0900

    [Gold IV] Title: 거짓말, Time: 132 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static int[] parent;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		
+		int N, M;
+		int inputCnt;
+		int prev, now;
+		int minParent;
+		int ans = 0;
+		boolean suc, isFalseExist;
+		List<List<Integer>> list = new ArrayList<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		parent = new int[N+1];
+		
+		for(int i = 0; i <= N; i++)
+			parent[i] = i;
+		for(int i = 0; i < M; i++) {
+			list.add(new ArrayList<>());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		inputCnt = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < inputCnt; i++) {
+			parent[Integer.parseInt(st.nextToken())] = 0;
+		}
+		
+		for(int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine());
+			inputCnt = Integer.parseInt(st.nextToken());
+			
+			for(int i = 0; i < inputCnt; i++) {
+				list.get(m).add(Integer.parseInt(st.nextToken()));
+			}
+			
+			isFalseExist = false;
+			minParent = Integer.MAX_VALUE;
+			for(int item: list.get(m)) {
+				if(getParent(item) == 0) {
+					isFalseExist = true;
+					break;
+				}
+				minParent = Math.min(minParent, getParent(item));
+			}
+			
+			if(isFalseExist) {
+				for(int item: list.get(m)) {
+					parent[getParent(item)] = 0;
+				}
+			}
+			else {
+				for(int item: list.get(m)) {
+					parent[getParent(item)] = minParent;
+				}
+			}
+		}
+		
+		for(List<Integer> party: list) {
+			suc = true;
+			for(int person: party) {
+				if(getParent(person) == 0) {
+					suc = false;
+					break;
+				}
+			}
+			if(suc)
+				ans += 1;
+		}
+		
+		System.out.println(ans);
+		
+//		System.out.println(Arrays.toString(parent));
+	}
+	
+	private static int getParent(int p) {
+		if(parent[p] == p)
+			return p;
+		
+		return getParent(parent[p]);
+	}
+
+	private static void unionParent(int p1, int p2) {
+		int a = getParent(p1);
+		int b = getParent(p2);
+		
+		if(a < b)
+			parent[p2] = a;
+		else {
+			parent[p1] = b;
+		}
+	}
+}
