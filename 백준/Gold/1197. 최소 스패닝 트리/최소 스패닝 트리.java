8a90ccf10391ef21aeb7c080f75a7e528eb01cb2
정승환 <100360525+seunghw2@users.noreply.github.com>
2024-10-03 16:55:06 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 640 ms, Memory: 56848 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,72 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-    public static int[] parent;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int V, E;
-        int a, b, w;
-        int tot = 0;
-        int[] pollItem;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-
-        parent = new int[V + 1];
-
-        for(int i = 1; i <= V; i++){
-            parent[i] = i;
-        }
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            w = Integer.parseInt(st.nextToken());
-
-            pq.add(new int[]{a, b, w});
-        }
-
-        while (!pq.isEmpty()) {
-            pollItem = pq.poll();
-
-            a = pollItem[0];
-            b = pollItem[1];
-            w = pollItem[2];
-
-//            System.out.println(parent[a] + " " + parent[b]);
-
-            if (getParent(a) != getParent(b)) {
-                unionParent(a, b);
-                tot += w;
-            }
-        }
-        System.out.println(tot);
-    }
-    public static int getParent(int x){
-        if(parent[x] == x){
-            return x;
-        }
-        else {
-            return getParent(parent[x]);
-        }
-    }
-
-    public static void unionParent(int x1, int x2) {
-        int p1 = getParent(x1);
-        int p2 = getParent(x2);
-
-        if(p1 > p2)
-            parent[p1] = p2;
-        else
-            parent[p2] = p1;
-    }
+	public static int[] parent;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int V, E;
+		int tot;
+		List<int[]> list = new ArrayList<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		tot = 0;
+		
+		parent = new int[V+1];
+		for(int i = 0; i <= V; i++) {
+			parent[i] = i;
+		}
+		
+		for(int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			list.add(new int[] {a, b, w});
+		}
+		
+		list = list.stream()
+				.sorted((o1, o2) -> o1[2] - o2[2])
+				.collect(Collectors.toList());
+		
+		for(int[] item: list) {
+			int a = item[0];
+			int b = item[1];
+			int w = item[2];
+			
+			if(getParent(a) == getParent(b))
+				continue;
+			
+			tot += w;
+			unionParent(a, b);
+		}
+	
+		System.out.println(tot);
+	}
+	
+	public static int getParent(int idx) {
+		if(idx == parent[idx])
+			return idx;
+		return parent[idx] = getParent(parent[idx]);
+	}
+	
+	public static void unionParent(int i1, int i2) {
+		int p1 = getParent(i1);
+		int p2 = getParent(i2);
+		
+		if(p1 < p2) {
+			parent[p1] = p2;
+		}
+		else {
+			parent[p2] = p1;
+		}
+	}
 }
